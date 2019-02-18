2018d28d7a4aabf562c02f1b27accd8d8af6be93
Robin Le Bihan <robin.le-bihan@epita.fr>
2019-02-18 07:48:49 +0100

    add small program example

--- src/sample.cc
+++ src/sample.cc
@@ -1,6 +1,39 @@
-#include <gsl/gsl-lite.hpp>
+#include <iostream>
 
-int main()
+#include <boost/program_options.hpp>
+
+namespace boostopt = boost::program_options;
+
+int main(int argc, char **argv)
 {
-    gsl::owner<int*> p = nullptr;
+    boostopt::options_description desc{"sample [--version]"};
+    desc.add_options()
+        ("help,h", "Display help message.")
+        ("version,v", "Display version information.\n");
+
+    boostopt::variables_map opts;
+    try {
+        boostopt::store(parse_command_line(argc, argv, desc), opts);
+        boostopt::notify(opts);
+    }
+    catch (const boostopt::unknown_option& exc)
+    {
+        std::cerr << "Error while parsing command line options: "
+                  << exc.what() << "\n\n" << desc << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    if (opts.count("help"))
+    {
+        std::cout << desc << std::endl;
+        return EXIT_SUCCESS;
+    }
+
+    if (opts.count("version"))
+    {
+        std::cout << "Version - 1.0.0" << std::endl;
+        return EXIT_SUCCESS;
+    }
+
+    return EXIT_SUCCESS;
 }

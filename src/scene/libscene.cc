7b748c0290b87f99b87c9f6a00ad0455841e8d5c
Robin Le Bihan <robin.le-bihan@epita.fr>
2019-02-19 20:31:34 +0100

    cmake: add scene library compilation

---
+++ src/scene/libscene.cc
@@ -0,0 +1,9 @@
+#include "libscene.hh"
+
+namespace scene
+{
+    Scene load_scene(const std::string& filename)
+    {
+        return Scene{};
+    }
+} // namespace scene
---
+++ src/scene/libscene.hh
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+
+#include "scene.hh"
+
+namespace scene
+{
+    Scene load_scene(const std::string& filename);
+} // namespace scene

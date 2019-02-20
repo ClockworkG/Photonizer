241d72a6e070722d9bf44fcaca5ad2a087bc7f2b
Robin Le Bihan <robin.le-bihan@epita.fr>
2019-02-20 07:58:12 +0100

    scene: refactor error handling

--- src/scene/libscene.cc
+++ src/scene/libscene.cc
@@ -18,14 +18,7 @@ namespace scene
 
         try {
             ptree::read_json(filename, scene_tree);
-        }
-        catch (const ptree::json_parser::json_parser_error& err)
-        {
-            std::cerr << "Syntax error: " << err.what() << '\n';
-            return nullptr;
-        }
 
-        try {
             builder.set_toplevel(scene_tree);
             builder.set_camera(scene_tree.get_child("camera"));
             builder.set_objects(scene_tree.get_child("objects"));
@@ -33,6 +26,11 @@ namespace scene
         catch (const ptree::ptree_bad_path& err)
         {
             std::cerr << "Ill-formed scene file: " << err.what() << '\n';
+            return nullptr;
+        }
+        catch (const ptree::json_parser::json_parser_error& err)
+        {
+            std::cerr << "Syntax error: " << err.what() << '\n';
             return nullptr;
         }
 

94953325bfeb4740648e7f7c6ebd89097751bf8f
Robin Le Bihan <robin.le-bihan@epita.fr>
2019-02-25 15:09:59 +0100

    image: add RGB Image

---
+++ src/image/image-rgb.hxx
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "image-rgb.hh"
+
+namespace image
+{
+    ImageRGB::ImageRGB(height_t height, width_t width)
+        : pixels_(height, typename pixels_t::value_type(width))
+    {}
+
+    inline
+    auto ImageRGB::operator[](const point_t& point) const -> const_reference
+    {
+        auto [x, y] = point;
+        return pixels_[x][y];
+    }
+
+    inline
+    auto ImageRGB::operator[](const point_t& point) -> reference
+    {
+        auto [x, y] = point;
+        return pixels_[x][y];
+    }
+
+    inline
+    auto ImageRGB::get_width() const -> width_t
+    {
+        return pixels_[0].size();
+    }
+
+    inline
+    auto ImageRGB::get_height() const -> height_t
+    {
+        return pixels_.size();
+    }
+
+} // namespace image

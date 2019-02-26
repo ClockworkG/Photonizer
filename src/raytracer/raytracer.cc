4854c218a14cd916b2052541e50abb3bbefca4ca
Alexandre Lamure <alexandre.lamure@epita.fr>
2019-02-26 20:21:44 +0100

    raytracer: add intersect func and dropped x_step and y_step

--- src/raytracer/raytracer.cc
+++ src/raytracer/raytracer.cc
@@ -1,11 +1,30 @@
 #include "raytracer.hh"
 
+#include "object.hh"
 #include "vector3.hh"
 #include "image-rgb.hh"
 #include "color.hh"
 
 namespace raytracer
 {
+    bool intersect(Scene scene, Ray ray)
+    {
+        for (auto object : scene)
+        {
+            // FIXME: check box intersection
+
+            for (auto polygon : object.get_mesh())
+            {
+                // triangle intersection
+                if (polygon.size() == 3)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     const ImageRGB& render(Scene scene)
     {
         using origin = scene.get_camera().get_position();
@@ -16,40 +35,35 @@ namespace raytracer
         using height = scene.get_height();
 
         // Create image buffer
-        ImageRGB buffer(scene.get_height(height, width));
-
-
+        ImageRGB img(scene.get_height(height, width));
 
         // Compute right unit vector
         Vector3f right = up ^ forward;
         // Compute screen center
-        Vector3f screen_center = origin + forward * zmin;
+        Vector3f screen_center = origin + forward * z_min;
         // Compute screen top left corner
         Vector3f screen_top_left = screen_center + up * (height / 2) - right * (width / 2);
-
-        // FIXME
-        // Compute incrementation step on X axis
-        Vector3f x_step = 2.0f * right / width;
-        // Compute incrementation step on y axis
-        Vector3f x_step = 2.0f * up / height;
 
         // Draw Loop
         for (int i = 0; i < height; ++i)
         {
             for (int j = 0; j < width; ++j)
             {
-                // FIXME
                 // Compute pixel position on the view plane
-                Vector3f pixel_pos = screen_top_left + i * y_step + j * x_step;
+                Vector3f target_pos = Vector3f(screen_top_left.x + i,
+                                              screen_top_left.y + j,
+                                              screen_top_left.z);
 
 
-                // FIXME
-                // Compute ray
-                Ray ray = Ray(origin, /*direction*/);
+                // Compute ray to cast from camera
+                Ray ray = Ray(origin, (pixel_pos - origin).normalize());
 
+                point_t pixel_pos = std::pair<height_t, width_t>(i, j);
                 // Test ray intersection
-
-                // If intersection, compute color
+                if (intersect(scene, ray))
+                    img[pixel_pos] = new Color(1.0f, 1.0f, 1.0f);
+                else
+                    img[pixel_pos] = new Color(0.0f, 0.0f, 0.0f);
             }
         }
     }

9e9e023dfd5d74a0e6d4a82503a001c0d298cb5d
Alexandre Lamure <alexandre.lamure@epita.fr>
2019-02-26 17:36:31 +0100

    raytracer: add source code

---
+++ src/raytracer/raytracer.cc
@@ -0,0 +1,55 @@
+#include "raytracer.hh"
+
+#include "vector3.hh"
+#include "color.hh"
+
+namespace raytracer
+{
+    const std::vector<std::vector<Color>>& render(Scene scene)
+    {
+        // Create image buffer
+        std::vector<std::vector<Color>> buffer(scene.get_height());
+        for (int i = 0; i < scene.get_height(); ++i)
+            buffer[i].resize(scene.get_width());
+
+
+        using origin = scene.get_camera().get_position();
+        using up = scene.get_camera().get_up();
+        using forward = scene.get_camera().get_forward();
+
+        using width = scene.get_width();
+        using height = scene.get_height();
+        // Compute right unit vector
+        Vector3f right = up ^ forward;
+        // Compute screen center
+        Vector3f screen_center = origin + forward * zmin;
+        // Compute screen top left corner
+        Vector3f screen_top_left = screen_center + up * (height / 2) - right * (width / 2);
+
+        // FIXME
+        // Compute incrementation step on X axis
+        Vector3f x_step = 2.0f * right / width;
+        // Compute incrementation step on y axis
+        Vector3f x_step = 2.0f * up / height;
+
+        // Draw Loop
+        for (int i = 0; i < height; ++i)
+        {
+            for (int j = 0; j < width; ++j)
+            {
+                // FIXME
+                // Compute pixel position on the view plane
+                Vector3f pixel_pos = screen_top_left + i * y_step + j * x_step;
+
+
+                // FIXME
+                // Compute ray
+                Ray ray = Ray(origin, /*direction*/);
+
+                // Test ray intersection
+
+                // If intersection, compute color
+            }
+        }
+    }
+}
---
+++ src/raytracer/raytracer.hh
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "scene.hh"
+
+namespace raytracer
+{
+    const std::vector<std::vector<Color>>& render(Scene scene);
+}

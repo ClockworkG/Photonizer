7eebbfeac5fd9f928deb869094e5c89844337dd0
Alexandre Lamure <alexandre.lamure@epita.fr>
2019-02-28 14:31:19 +0100

    raytracer: moller trumbore intersection

--- src/raytracer/raytracer.cc
+++ src/raytracer/raytracer.cc
@@ -1,5 +1,7 @@
 #include "raytracer.hh"
 
+#include <cmath>
+
 #include "object.hh"
 #include "vector3.hh"
 #include "ray.hh"
@@ -7,22 +9,45 @@
 
 namespace raytracer
 {
+    #define epsilon 0.0001
+
+    bool moller_trumbore(scene::Mesh::polygon_t polygon, Rayf ray, float &t)
+    {
+        Vector3f ab_v = polygon[1].first - polygon[0].first;
+        Vector3f ac_v = polygon[2].first - polygon[0].first;
+        Vector3f p_vec = ray.dir ^ ac_v;
+        float det = ab_v * p_vec;
+
+        if (std::abs(det) < epsilon)
+            return false;
+
+        float inv_det = 1 / det;
+        Vector3f t_vec = ray.o - polygon[0].first;
+        float bary_u = t_vec * p_vec * inv_det;
+        if (bary_u < 0 || bary_u > 1)
+            return false;
+
+        Vector3f q_vec = t_vec * ab_v;
+        float bary_v = ray.dir * q_vec * inv_det;
+        if (bary_v < 0 || bary_u + bary_v > 1)
+            return false;
+
+        t = ac_v * q_vec * inv_det;
+
+        return true;
+    }
+
     bool intersect(scene::Scene scene, Rayf ray)
     {
+        float t = 0;
         for (auto object : scene)
         {
             // FIXME: check box intersection
 
             for (auto polygon : object.get_mesh())
             {
-                // triangle intersection
-                if (polygon.size() == 3)
-                {
-                    // avoid warning
-                    if (ray.o == ray.dir)
-                        return false;
+                if (moller_trumbore(polygon, ray, t))
                     return true;
-                }
             }
         }
         return false;

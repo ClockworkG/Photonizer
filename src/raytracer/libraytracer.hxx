61646e7797a53800e08e81d2ecac85b4753de4bf
Alexandre Lamure <alexandre.lamure@epita.fr>
2019-04-26 17:02:54 +0200

    raytracer/multithread: try with openmp lib

--- src/raytracer/libraytracer.hxx
+++ src/raytracer/libraytracer.hxx
@@ -3,7 +3,7 @@
 #include "libraytracer.hh"
 
 #include <spdlog/spdlog.h>
-#include <future>
+#include <omp.h>
 
 namespace raytracer
 {
@@ -41,52 +41,24 @@ namespace raytracer
 
             Tracer ray_cast(scene, config, std::move(photon_map));
 
-            // Multithreading
-            size_t nb_pixels = img_width * img_height;
-            std::vector<typename Tracer::value_type> img_flat(nb_pixels);
-            size_t nb_cores = std::thread::hardware_concurrency();
-            std::vector<std::future<void>> future_vector;
-
-            for (size_t core_id = 0; core_id < nb_cores; ++core_id)
+            // Draw Loop
+            #pragma omp parallel for
+            for (int y = 0; y < img_height; ++y)
             {
-                future_vector.emplace_back(std::async([=]()
+                for (int x = 0; x < img_width; ++x)
                 {
-                    for (size_t index = core_id; index < nb_pixels; index += nb_cores)
-                    {
-                        size_t x = index % (int)img_width;
-                        size_t y = index / (int)img_width;
-                        /*
-                        // Draw Loop
-                        for (int y = 0; y < img_height; ++y)
-                        {
-                            for (int x = 0; x < img_width; ++x)
-                            {
-                            */
-                                // x and y are expressed in raster space
-                                // Screen space coordinates are in range [-1, 1]
-                                float screen_x = (2.0 * (x + 0.5) / img_width - 1.0) * coef_x;
-                                float screen_y = (1.0 - 2.0 * (y + 0.5) / img_height) * coef_y;
-                                Vector3f target_pos = Vector3f(screen_x, screen_y, origin.z + z_min);
+                    // x and y are expressed in raster space
+                    // Screen space coordinates are in range [-1, 1]
+                    float screen_x = (2.0 * (x + 0.5) / img_width - 1.0) * coef_x;
+                    float screen_y = (1.0 - 2.0 * (y + 0.5) / img_height) * coef_y;
+                    Vector3f target_pos = Vector3f(screen_x, screen_y, origin.z + z_min);
 
-                                // Compute ray to cast from camera
-                                Ray ray = Ray(origin, (target_pos - origin).normalize());
+                    // Compute ray to cast from camera
+                    Ray ray = Ray(origin, (target_pos - origin).normalize());
 
-                                img_flat[index] = ray_cast(ray);
-                            /*
-                            }
-                        }
-                        */
-                    }
-
-                }));
-            }
-            for (size_t i = 0; i < nb_pixels; ++i)
-            {
-                size_t x = i % (int)img_width;
-                size_t y = i / (int)img_width;
-
-                auto pixel_pos = std::pair(y, x);
-                img[pixel_pos] = img_flat[i];
+                    auto pixel_pos = std::pair(y, x);
+                    img[pixel_pos] = ray_cast(ray);
+                }
             }
         }
 
